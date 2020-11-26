3838ab632f8c510672e868fac8e298e7b6839431
Arc Coder | Harsh Patel <dev@arccoder.in>
2020-11-26 18:52:24 +0530

    Created basic server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+require("colors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+// * Initialize Express server
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`.red);
+});
